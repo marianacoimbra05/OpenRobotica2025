75e85d5a202f6beacfc02f80113fd02ead71f234
rodrigo.c.oliveira <rodrigo.c.oliveira@inesctec.pt>
2025-03-21 14:56:05 +0000

    Com PID

--- Carro_v0/Carro_v0.ino
+++ Carro_v0/Carro_v0.ino
@@ -1,6 +1,26 @@
+#include <Arduino.h>
+#include <esp_timer.h>
 
-const int DEBUG = 1;
-const int delaytime = 2;
+esp_timer_handle_t timer;
+
+
+const int DEBUG = 0;
+const int delaytime = 5;
+/*         PID         */
+const float Kp = 2;
+const float Kd = .5;
+const float Ki = .2;
+
+
+float P, I, D, PID;
+
+float lsp, rsp;
+
+float error = 0, lastError = 0;
+float max_windup = 50;
+
+void timer_callback(void* arg);
+/*       END OF PID      */
 
 /*     MOTORS       */
 const int pin_MotorR_1 = 27;    
@@ -20,11 +40,11 @@ const int Motor_enable = 25;
 const int resolution = 12;
 const float maxpwm = 4095 - 1;
 
-volatile float Percent_R = 0;
-volatile float Percent_L = 0;
+const float MAX_PERCENT_R = 100;
+const float MAX_PERCENT_L = 100;
 
-const float MAX_PERCENT_R = 40;
-const float MAX_PERCENT_L = 42.5;
+volatile float Percent_R = MAX_PERCENT_R/2;
+volatile float Percent_L = MAX_PERCENT_L/2;
 
 const float MIN_PERCENT_R = 10;
 const float MIN_PERCENT_L = 10;
@@ -36,23 +56,23 @@ const float MIN_PERCENT_L = 10;
 const int IR_0 = 21;
 const int IR_1 = 19;
 const int IR_2 = 18;
-const int IR_3 = 5;
+const int IR_3 = 16;
 const int IR_4 = 17;
 
 int readings[5] = {1, 1, 0, 1, 1};
 
 const float bigDeviation = .30;
-const float smallDeviation = .16;  
+const float smallDeviation = .2;  
 
 /*    END OF IR SENSOR   */
 
 /*         LDR           */
 
-const int LDR_0 = 15;
+const int LDR_0 = 5;
 const int LDR_1 = 2;
 const int LDR_2 = 0;
 
-const int LDR_Go = 900;
+const int LDR_Go = 90000;
 
 int LDR_value[3] = {0,0,0};
 
@@ -60,23 +80,31 @@ int LDR_value[3] = {0,0,0};
 
 
 
+
+
+
 /*         Function declaration         */
-void set_pwm(float pwm, char dir, char motor);
+void set_pwm(float pwm, char motor);
 int percent_to_pwm(float percent);
-int read_IR();
-int control();
+void read_IR();
+int control_if();
+void control_PID();
+void calc_PID();
+void go();
 
-/*
-void IRAM_ATTR isr() {
-	button1.numberKeyPresses++;
-	button1.pressed = true;
-}
-*/
-
-bool start=true;
 
 void setup() 
 {
+    // Configure the timer
+  esp_timer_create_args_t timer_args = {
+    .callback = &timer_callback,
+    .name = "periodic_timer"
+  };
+
+  esp_timer_create(&timer_args, &timer);
+
+  // Start the timer (10ms = 10000 microseconds)
+  esp_timer_start_periodic(timer, delaytime * 1000);
 
   //MOTORS
   
@@ -93,44 +121,44 @@ void setup()
   ledcAttachChannel(pin_MotorL_2, 500, resolution, MotorL_2);
 
   //Initialize motors at 
-  set_pwm(MAX_PERCENT_L/2, 'F', 'L');
-  set_pwm(MAX_PERCENT_R/2, 'F', 'R');
+  set_pwm(0, 'L');
+  set_pwm(0, 'R');
+
+
+  digitalWrite(Motor_enable, 1);
 
   //END OF MOTORS
 
 
   //IR SENSOR
-  pinMode(IR_0, INPUT);
-  pinMode(IR_1, INPUT);
-  pinMode(IR_2, INPUT);
-  pinMode(IR_3, INPUT);
-  pinMode(IR_4, INPUT);
+  pinMode(IR_0, INPUT_PULLUP);
+  pinMode(IR_1, INPUT_PULLUP);
+  pinMode(IR_2, INPUT_PULLUP);
+  pinMode(IR_3, INPUT_PULLUP);
+  pinMode(IR_4, INPUT_PULLUP);
 
   //END OF IR SENSOR
 
 
   //LDR
 
-  pinMode(LDR_0, OUPUT);
+  pinMode(LDR_0, OUTPUT);
   pinMode(LDR_1, INPUT);
-  pinMode(LDR_2, INPUT_PULLDOWN);
+  pinMode(LDR_2, OUTPUT);
 
+  digitalWrite(LDR_0, 1);
+  digitalWrite(LDR_0, 0);
+  
   //END OF LDR
 
-
-//
 
 Serial.begin(115200);
 }
 
 void loop() 
 {
-  
-  if(start==true){
-    LDR_value[1] = analogRead(LDR_1); 
-    go();
-    start=false;
-  }
+  /*
+  go();
 
   read_IR();
 
@@ -151,7 +179,17 @@ void loop()
   }
 
   delay(delaytime);
+  */
 
+
+
+}
+
+void timer_callback(void* arg) 
+{
+  read_IR();
+  calc_PID();
+  control_PID();
 }
 
 /*
@@ -159,11 +197,12 @@ percent 0 to 100
 dir F or B
 motor R or L
 */
-void set_pwm(float percent, char dir, char motor)
+void set_pwm(float percent, char motor)
 {
   
   if (percent > MAX_PERCENT_R and motor == 'R') percent = MAX_PERCENT_R;
   if (percent > MAX_PERCENT_L and motor == 'L') percent = MAX_PERCENT_L;
+
 
 
 
@@ -173,8 +212,6 @@ void set_pwm(float percent, char dir, char motor)
   {
     Serial.print("Setting");
     Serial.print(motor);
-    Serial.print(" to: ");
-    Serial.print(dir);
     Serial.print(" at pc: ");
     Serial.print(percent);
     Serial.print(" pwm: ");
@@ -182,33 +219,38 @@ void set_pwm(float percent, char dir, char motor)
   }
 
 
-  if( dir == 'F' and motor == 'R' )
+  if(motor == 'R' )
   {
+    if (percent >= 0)
+    {
+      ledcWrite(pin_MotorR_1 , pwm);
+      ledcWrite(pin_MotorR_2 , 0);  
+    }
+
+    if (percent < 0)
+    {
+      ledcWrite(pin_MotorR_1 , 0);
+      ledcWrite(pin_MotorR_2 , pwm);  
+    }
     
-    ledcWrite(pin_MotorR_1 , pwm);
-    ledcWrite(pin_MotorR_2 , 0);
   }
 
-  if( dir == 'F' and motor == 'L' )
+  if(motor == 'L' )
   {
+    if (percent >= 0)
+    {
+      ledcWrite(pin_MotorL_1 , pwm);
+      ledcWrite(pin_MotorL_2 , 0);  
+    }
 
-    ledcWrite(pin_MotorL_1 , pwm);
-    ledcWrite(pin_MotorL_2 , 0);
+    if (percent < 0)
+    {
+      ledcWrite(pin_MotorL_1 , 0);
+      ledcWrite(pin_MotorL_2 , pwm);  
+    }
+    
   }
 
-  if( dir == 'B' and motor == 'R' )
-  {
-
-    ledcWrite(pin_MotorR_1 , 0);
-    ledcWrite(pin_MotorR_2 , pwm);
-  }
-
-  if( dir == 'B' and motor == 'L' )
-  {
-
-    ledcWrite(pin_MotorL_1 , 0);
-    ledcWrite(pin_MotorL_2 , pwm);
-  }
 }
 
 int percent_to_pwm(float percent)
@@ -222,29 +264,31 @@ int percent_to_pwm(float percent)
   return pwm;
 }
 
-int read_IR()
+void read_IR()
 {
 
-  int reading0 = digitalRead(IR_0);
-  int reading1 = digitalRead(IR_1);
-  int reading2 = digitalRead(IR_2);
-  int reading3 = digitalRead(IR_3);
-  int reading4 = digitalRead(IR_4); 
+  readings[0] = digitalRead(IR_0); ;
+  readings[1] = digitalRead(IR_1); ;
+  readings[2] = digitalRead(IR_2); ;
+  readings[3] = digitalRead(IR_3); ;
+  readings[4] = digitalRead(IR_4); ;
 
-
-  if (reading0 == 1 and reading1 == 1 and reading2 == 1 and reading3 == 1 and reading4 == 1) return 0;
-  
-  readings[0] = reading0;
-  readings[1] = reading1;
-  readings[2] = reading2;
-  readings[3] = reading3;
-  readings[4] = reading4;
+  if(DEBUG)
+  {
+    Serial.print  (readings[0]);
+    Serial.print  (" ");
+    Serial.print  (readings[1]);
+    Serial.print  (" ");
+    Serial.print  (readings[2]);
+    Serial.print  (" ");
+    Serial.print  (readings[3]);
+    Serial.print  (" ");
+    Serial.println(readings[4]);
+  }
 
 }
 
-
-
-int control()
+int control_if()
 {
   //Left in the black line
   //Correct to right 
@@ -256,12 +300,12 @@ int control()
     if (Percent_L - bigDeviation > MIN_PERCENT_L)
     {
       Percent_L = Percent_L - bigDeviation;
-      set_pwm(Percent_L, 'F', 'L');
+      set_pwm(Percent_L, 'L');
     }
     if (Percent_R + bigDeviation < MAX_PERCENT_R)
     {
       Percent_R = Percent_R + bigDeviation;
-      set_pwm(Percent_R, 'F', 'R');
+      set_pwm(Percent_R, 'R');
     }
   }
 
@@ -272,12 +316,12 @@ int control()
     if (Percent_L - smallDeviation > MIN_PERCENT_L)
     { 
       Percent_L = Percent_L - smallDeviation;
-      set_pwm(Percent_L, 'F', 'L');
+      set_pwm(Percent_L, 'L');
     }
     if (Percent_R + smallDeviation < MAX_PERCENT_R)
     {
       Percent_R = Percent_R + smallDeviation;
-      set_pwm(Percent_R, 'F', 'R');
+      set_pwm(Percent_R, 'R');
     }
   }
 
@@ -289,13 +333,13 @@ int control()
     if (Percent_R + bigDeviation< MAX_PERCENT_R)
     {
       Percent_R = Percent_R + bigDeviation;
-      set_pwm(Percent_R, 'F', 'R');
+      set_pwm(Percent_R, 'R');
     }
 
     if (Percent_L + bigDeviation < MAX_PERCENT_L)
     {
       Percent_L = Percent_L + bigDeviation;
-      set_pwm(Percent_L, 'F', 'L');
+      set_pwm(Percent_L, 'L');
     }
   }
 
@@ -310,12 +354,12 @@ int control()
     if (Percent_L + smallDeviation < MAX_PERCENT_L)
     {
       Percent_L = Percent_L + smallDeviation;
-      set_pwm(Percent_L, 'F', 'L');
+      set_pwm(Percent_L, 'L');
     }
     if (Percent_R - smallDeviation > MIN_PERCENT_R)
     {
       Percent_R = Percent_R - smallDeviation;
-      set_pwm(Percent_R, 'F', 'R');
+      set_pwm(Percent_R, 'R');
     }
   }
 
@@ -327,12 +371,12 @@ int control()
     if (Percent_L + bigDeviation < MAX_PERCENT_L)
     {
       Percent_L = Percent_L + bigDeviation;
-      set_pwm(Percent_L, 'F', 'L');
+      set_pwm(Percent_L, 'L');
     }
     if (Percent_R - bigDeviation > MIN_PERCENT_R)
     {
       Percent_R = Percent_R - bigDeviation;
-      set_pwm(Percent_R, 'F', 'R');
+      set_pwm(Percent_R, 'R');
     }
   }
 
@@ -343,8 +387,8 @@ int control()
 
     Percent_R = 0;
     Percent_L = 0;
-    set_pwm(Percent_R, 'F', 'R');
-    set_pwm(Percent_L, 'F', 'L');
+    set_pwm(Percent_R, 'R');
+    set_pwm(Percent_L, 'L');
     return 0;
   }
 
@@ -352,10 +396,84 @@ int control()
 
 }
 
+void calc_PID()
+{
+
+  error = (readings[0] * (-2) + readings[1] * (-1) + readings[2] * (0) + readings[3] * (1) + readings[4] * (2));
+
+
+  P = error;
+  I = I + error;
+  D = lastError - error;
+  lastError = error;
+  if(I > max_windup) I = max_windup;
+  if(I < -max_windup) I = -max_windup;
+
+  PID = P * Kp + I * Ki + D * Kd;
+
+  if (DEBUG) 
+  {
+    Serial.print("P: ");
+    Serial.println(P);
+    Serial.print("I: ");
+    Serial.println(I);
+    Serial.print("D: ");
+    Serial.println(D);
+    Serial.print("PID: ");
+    Serial.println(PID);
+  }
+}
+
+void control_PID()
+{
+  
+  if(PID > 0)
+  { //Virar à esquerda
+    
+    Percent_L = Percent_L + PID; //motor esquerdo mais lento
+    Percent_R = Percent_R - PID; //motor direito mais rápido
+
+  } else if (PID < 0)
+  { //virar à direita
+
+    Percent_L = Percent_L + PID; //motor esquerdo mais rápido
+    Percent_R = Percent_R - PID; //motor direito mais lento
+
+  } 
+
+  if (error == 0)
+  {
+    Percent_L = Percent_L + smallDeviation;
+    Percent_R = Percent_R + smallDeviation;
+  }
+
+
+  set_pwm(Percent_L, 'L' );
+  set_pwm(Percent_R, 'R' );
+
+
+}
+
 void go()
 {
-  if (LDR_value[1] > LDR_Go){
-    digitalWrite(Motor_enable, HIGH);
+  int LDR_value = 0;
+
+  while(1)
+  {
+
+    LDR_value = analogRead(LDR_1);
+
+    if (DEBUG) 
+    {
+    Serial.print("LDR: ");
+    Serial.println(LDR_value);
+    }
+
+    if (LDR_value > LDR_Go){
+      digitalWrite(Motor_enable, 1);
+      break;
+    } 
+
   }
 
 }
